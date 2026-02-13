ebb7f4cb48268623eb9cb2daacbdb2c64d658eca
qu1sty <ameserakov304@gmail.com>
2026-02-14 03:10:32 +0700

    cards

--- js/main.js
+++ js/main.js
@@ -1,3 +1,42 @@
+Vue.component('note-card', {
+    props: {
+        card: {
+            type: Object,
+            required: true
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        }
+    },
+    methods: {
+        emitUpdate() {
+            this.$emit('update', this.card);
+        }
+    },
+    template: `
+        <article class="note-card">
+            <h3>{{ card.title }}</h3>
+            <ul>
+                <li v-for="(item, index) in card.items" :key="index">
+                    <label>
+                        <input
+                            type="checkbox"
+                            v-model="item.done"
+                            :disabled="disabled"
+                            @change="emitUpdate"
+                        >
+                        {{ item.text }}
+                    </label>
+                </li>
+            </ul>
+            <div v-if="card.completedAt" class="note-card-date">
+                Дата завершения: {{ card.completedAt }}
+            </div>
+        </article>
+    `
+});
+
 new Vue({
     el: '#app',
     data() {
